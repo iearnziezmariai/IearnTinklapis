54415e1f66385b2b008ac96c3c0504162fd3e8c2
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2021-04-18 00:23:24 +0300

    Gyčio Narių sekcijos tvarkymai
    Pakeičiau fontą
    Įdėjau background spalvą #f1f1f1
    Sutvarkiau telefono versiją kad būtų 3 žmonių eilutės

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,35 +1,50 @@
 $(document).ready(function(){
     $('#Nariai .container-3 .carousel').slick({
+        rows: 2,
         draggable: false,
         autoplay: true,
         autoplaySpeed: 5000,
         speed: 1000,
         dots: true,
         slidesToShow: 5,
-        slidesToScroll: 1,
+        slidesToScroll: 5,
         prevArrow: '<span class="priv_arrow"><img src="../images/left button.svg" alt="previous"></i></span>',
         nextArrow: '<span class="next_arrow"><img src="../images/right button.svg" alt="next"></span>',
         
         responsive: [
             {
-                // screens greater than >= 1200px
                 breakpoint: 1600,
                 settings: {
                     slidesToShow: 4,
+                    slidesToScroll: 4,
                 }
             },
             {
                 breakpoint: 1200,
                 settings: {
+                    rows: 2,
                     slidesToShow: 3,
+                    slidesToScroll: 3,
                 }
             },
             {
-                breakpoint: 860,
+                breakpoint: 700,
                 settings: {
+                    rows: 2,
                     arrows: false,
                     draggable: true,
                     slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 380,
+                settings: {
+                    rows: 3,
+                    arrows: false,
+                    draggable: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
             }
         ]
